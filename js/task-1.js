472248575a594720f61ec41f65fe2dbef7648d82
Serhii Bakhmatov <bakhmatov1982@gmail.com>
2025-01-24 10:24:35 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  const makeTransaction = `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
+  return makeTransaction;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
