d5ab1d17cc2aa3bdeacec8c61cfa8048d0dfb2b5
bahmat <bakhmatov1982@gmail.com>
2025-01-26 15:17:24 +0200

    add task-3,4

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,8 @@
 function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return (formatMessage = `"${message.slice()}"`);
-  } else {
-    return (formatMessage = `"${message.slice(message, maxLength)}..."`);
-  }
+  return (result =
+    message.length <= maxLength
+      ? `"${message.slice()}"`
+      : `"${message.slice(0, maxLength)}..."`);
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
